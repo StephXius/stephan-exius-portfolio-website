63d8cd41cc123d6f8e3b08e057b1192b8383a8e0
StephXius <stephanexius1@gmail.com>
2023-01-11 03:09:49 -0500

    test commit seven

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,58 @@
-import React from 'react'
-import { Container } from 'react-bootstrap'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import headerimg from "../assets/img/header-img.svg";
 
 export const Banner = () => {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const toRotate = ["Frontend Developer", "Software Engineer"];
+    const [text,setText] = useState('');
+    const [delta, setDelta] = useState(300 - Math.random() * 100);
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta)
+
+        return () => {clearInterval(ticker)}
+    }, [text])
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+
+        setText(updatedText);
+
+        if (isDeleting) {
+            setDelta(prevData => prevData / 2)
+        }
+
+        if (isDeleting && updatedText === fullText){
+            setIsDeleting(true);
+            setDelta(period);
+        } else if (isDeleting && updatedText === ''){
+            setIsDeleting(false);
+            setLoopNum(loopNum + 1);
+            setDelta(500);
+        }
+    }
+
     return (
         <section className='banner' id='home'>
             <Container>
                 <Row className='align-items-center'>
-                    <Col xs={12}>
-
+                    <Col xs={12} md={6} xl={7}>
+                       <span className='tagline'>Welcome to my Portfolio</span>
+                       <h1>{`Hi I'm Stephan`}<span className='wrap'>[text]</span></h1>
+                       <p>I am a senior college student who loves coding and is a aspiring Software Engineer. I've learned HTML, CSS, JavaScript, React, SQL, Java and C# throughout my year of coding.</p>
+                       <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}/></button>
+                    </Col>
+                    <Col xs={12} md={6} xl={5}>
+                        <img src={headerimg} alt='Header Img' />
                     </Col>
                 </Row>
             </Container>
